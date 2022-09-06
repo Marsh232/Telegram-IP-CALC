58bc31adee307d73d3eeeb2233467c6d7e1ebb7d
Marsh232 <trunov112004@gmail.com>
2022-09-07 01:08:57 +0300

    Добавил комменты и пару строчек

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import ipaddress
 
 ip = input('Введите ip: ') # Пользователь вводит ip
 
-list_ip = ip.split('/')
+list_ip = ip.split('/') # Разделяет вводимый ip на часть с маской, и без
 ipv4 = ipaddress.ip_address(list_ip[0])
-print(ipv4)
-net = ipaddress.ip_network('192.168.10.0/24') # В функцию кладётся сетевая часть ip, без хостовой части
+print('Ваш ip адрес:', ip) # Выводит например "Ваш ip адрес: 192.168.10.128/24"
+
+net = ipaddress.ip_network(ip, strict=False) # В функцию кладётся сетевая часть ip, без хостовой части
+print(net)
 print(net.netmask)
