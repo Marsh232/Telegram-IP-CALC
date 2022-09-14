e67321984cc5a3310872c3f45201d3f92898eed0
SantaSpeen <SantaSpeen@yandex.ru>
2022-09-14 13:15:11 +0300

    Del

--- src/keyboard.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-
-network_button = KeyboardButton('Сеть')
-subnet_button = KeyboardButton('Подсети')
-help_button = KeyboardButton('Помощь')
-
-kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kb.row(network_button, subnet_button, help_button)
